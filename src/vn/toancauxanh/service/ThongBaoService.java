a41e4e097dc76d01899c9359e6f439f1f2827e1f
DongVu <hieutd@greenglobal.vn>
2018-11-22 09:33:27 +0700

    feat(ThongBao) thong bao tu dong
    see#51

--- src/vn/toancauxanh/service/ThongBaoService.java
+++ src/vn/toancauxanh/service/ThongBaoService.java
@@ -1,30 +1,20 @@
 package vn.toancauxanh.service;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.collections.MapUtils;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 
 import com.querydsl.jpa.impl.JPAQuery;
 
-import vn.toancauxanh.gg.model.enums.LoaiCongViec;
 import vn.toancauxanh.gg.model.enums.LoaiThongBao;
-import vn.toancauxanh.gg.model.enums.TrangThaiGiaoViec;
-import vn.toancauxanh.model.GiaoViec;
-import vn.toancauxanh.model.QGiaoViec;
 import vn.toancauxanh.model.QThongBao;
 import vn.toancauxanh.model.ThongBao;
 
-@Configuration
-@EnableScheduling
 public class ThongBaoService extends BasicService<ThongBao> {
 	public JPAQuery<ThongBao> getTargetQuery() {
 		String param = MapUtils.getString(argDeco(), "tuKhoa", "").trim();
@@ -36,7 +26,6 @@ public class ThongBaoService extends BasicService<ThongBao> {
 			String tuKhoa = "%" + param + "%";
 			q.where(QThongBao.thongBao.noiDung.like(tuKhoa));
 		}
-
 		if (loaiThongBao != null && !loaiThongBao.isEmpty()) {
 			q.where(QThongBao.thongBao.loaiThongBao.eq(LoaiThongBao.valueOf(loaiThongBao)));
 		}
@@ -49,29 +38,6 @@ public class ThongBaoService extends BasicService<ThongBao> {
 		return q;
 	}
 
-	@Scheduled(cron = "0 0 12 * * *")
-	public void kiemTraVaGuiThongBaoDenHanCongViec() {
-		JPAQuery<GiaoViec> q = find(GiaoViec.class)
-				.where(QGiaoViec.giaoViec.trangThaiGiaoViec.ne(TrangThaiGiaoViec.HOAN_THANH))
-				.where(QGiaoViec.giaoViec.hanThucHien.eq(resetHourMinuteSecondMilli(new Date())));
-		if (q.fetchCount() > 0) {
-			q.fetch().forEach(item -> {
-				ThongBao thongBao = new ThongBao();
-				thongBao.setNoiDung("Công việc @" + item.getTenCongViec()
-								+ "@ đã đến hạn hoàn thành.");
-				thongBao.setNguoiNhan(item.getNguoiDuocGiao());
-				if (LoaiCongViec.DU_AN.equals(item.getLoaiCongViec())) {
-					thongBao.setIdObject(item.getDuAn().getId());
-				} else {
-					thongBao.setIdObject(item.getDoanVao().getId());
-				}
-				thongBao.setLoaiThongBao(LoaiThongBao.DEN_HAN_CONG_VIEC);
-				thongBao.saveNotShowNotification();
-			});
-		}
-		
-	}
-	
 	public Long getSizeNewNotify() {
 		return getTargetQuery().where(QThongBao.thongBao.daXem.eq(false)).fetchCount();
 	}
@@ -100,4 +66,5 @@ public class ThongBaoService extends BasicService<ThongBao> {
 		result.put("true", "Đã xem");
 		return result;
 	}
+	
 }
---
+++ src/vn/toancauxanh/service/thongbao/ThongBaoTuDongService.java
@@ -0,0 +1,44 @@
+package vn.toancauxanh.service.thongbao;
+
+import java.util.Date;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import com.querydsl.jpa.impl.JPAQuery;
+
+import vn.toancauxanh.gg.model.enums.LoaiCongViec;
+import vn.toancauxanh.gg.model.enums.LoaiThongBao;
+import vn.toancauxanh.gg.model.enums.TrangThaiGiaoViec;
+import vn.toancauxanh.model.GiaoViec;
+import vn.toancauxanh.model.QGiaoViec;
+import vn.toancauxanh.model.ThongBao;
+import vn.toancauxanh.service.BasicService;
+
+@Configuration
+@EnableScheduling
+public class ThongBaoTuDongService extends BasicService<ThongBao>{
+	@Scheduled(cron = "0 0 12 * * *")
+	public void kiemTraVaGuiThongBaoDenHanCongViec() {
+		JPAQuery<GiaoViec> q = find(GiaoViec.class)
+				.where(QGiaoViec.giaoViec.trangThaiGiaoViec.ne(TrangThaiGiaoViec.HOAN_THANH))
+				.where(QGiaoViec.giaoViec.hanThucHien.eq(resetHourMinuteSecondMilli(new Date())));
+		if (q.fetchCount() > 0) {
+			q.fetch().forEach(item -> {
+				ThongBao thongBao = new ThongBao();
+				thongBao.setNoiDung("Công việc @" + item.getTenCongViec()
+								+ "@ đã đến hạn hoàn thành.");
+				thongBao.setNguoiNhan(item.getNguoiDuocGiao());
+				if (LoaiCongViec.DU_AN.equals(item.getLoaiCongViec())) {
+					thongBao.setIdObject(item.getDuAn().getId());
+				} else {
+					thongBao.setIdObject(item.getDoanVao().getId());
+				}
+				thongBao.setLoaiThongBao(LoaiThongBao.DEN_HAN_CONG_VIEC);
+				thongBao.saveNotShowNotification();
+			});
+		}
+		
+	}
+}
