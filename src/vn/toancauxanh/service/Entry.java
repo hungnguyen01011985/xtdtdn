40eb4f2d16754335627a42b4ca3dcd9f370ea664
DongVu <hieutd@greenglobal.vn>
2018-12-03 14:43:53 +0700

    fix(GiaoViec) fix quyen giao viec
     fix quyen nhac giao giao viec
    
    see#70'

--- src/vn/toancauxanh/service/Entry.java
+++ src/vn/toancauxanh/service/Entry.java
@@ -133,8 +133,6 @@ public class Entry extends BaseObject<Object> {
 	public String QUANLYDOANVAOSUA;
 	@Value("${url.quanlydoanvao}" + ":" + "${action.xoa}")
 	public String QUANLYDOANVAOXOA;
-	@Value("${url.quanlydoanvao}" + ":" + "${action.nhacnho}")
-	public String QUANLYDOANVAONHACNHO;
 
 	@Value("${url.quanlygiaoviec}" + ":" + "${action.list}")
 	public String QUANLYGIAOVIECLIST;
@@ -146,6 +144,8 @@ public class Entry extends BaseObject<Object> {
 	public String QUANLYGIAOVIECXOA;
 	@Value("${url.quanlygiaoviec}" + ":" + "${action.them}")
 	public String QUANLYGIAOVIECTHEM;
+	@Value("${url.quanlygiaoviec}" + ":" + "${action.nhacnho}")
+	public String QUANLYGIAOVIECNHACNHO;
 	
 
 	@Value("${url.phongban}" + ":" + "${action.list}")
