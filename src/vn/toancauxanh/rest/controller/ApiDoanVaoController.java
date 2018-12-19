966dd6f04584411e62b5954fac1e2a14e944b369
Dong Vu <hieutd@greenglobal.vn>
2018-12-19 08:57:24 +0700

    feat(DuAn): trich xuat api du an
    trich xuat api du an
    
    see#80

--- src/vn/toancauxanh/rest/controller/ApiDoanVaoController.java
+++ src/vn/toancauxanh/rest/controller/ApiDoanVaoController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import vn.toancauxanh.model.DoanVao;
 import vn.toancauxanh.rest.model.DoanVaoModel;
 import vn.toancauxanh.rest.model.PagingObject;
 import vn.toancauxanh.rest.service.DoanVaoModelService;
+import vn.toancauxanh.rest.service.KeyApiService;
 
 @RestController
 @RequestMapping(path = "/api/v1/doanVaos", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,18 +25,33 @@ public class ApiDoanVaoController {
 	@Autowired
 	private DoanVaoModelService doanVaoModelService;
 	
+	@Autowired
+	private KeyApiService keyApiService;
+	
 	@GetMapping
-    public PagingObject<DoanVaoModel> findAll(Pageable pageable, @RequestParam(required = false, defaultValue = "") String keyWord) {
-        return doanVaoModelService.doanVaos(pageable, keyWord);
-    }
+	public ResponseEntity<PagingObject<DoanVaoModel>> findAll(
+			@RequestHeader(value = "authentication", required = true) String authentication, Pageable pageable,
+			@RequestParam(required = false, defaultValue = "") String keyWord) {
+		boolean checkQuyen = keyApiService.checkKey(authentication);
+		if (checkQuyen) {
+			return new ResponseEntity<>(doanVaoModelService.doanVaos(pageable, keyWord), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<DoanVaoModel> show(@PathVariable("id") Long id) {
-		DoanVao doanVao = doanVaoModelService.getById(id);
-		if (doanVao != null) {
-			return new ResponseEntity<>(doanVao.toDoanVaoModel(), HttpStatus.OK);
+	public ResponseEntity<DoanVaoModel> show(@PathVariable("id") Long id,
+			@RequestHeader(value = "authentication", required = true) String authentication) {
+		boolean checkQuyen = keyApiService.checkKey(authentication);
+		if (checkQuyen) {
+			DoanVao doanVao = doanVaoModelService.getById(id);
+			if (doanVao != null) {
+				return new ResponseEntity<>(doanVao.toDoanVaoModel(), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			}
 		}
-		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
 	}
 	
 }
--- src/vn/toancauxanh/rest/controller/ApiDuAnController.java
+++ src/vn/toancauxanh/rest/controller/ApiDuAnController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import vn.toancauxanh.model.DuAn;
 import vn.toancauxanh.rest.model.DuAnModel;
 import vn.toancauxanh.rest.model.PagingObject;
 import vn.toancauxanh.rest.service.DuAnModelService;
+import vn.toancauxanh.rest.service.KeyApiService;
 
 @RestController
 @RequestMapping(path = "/api/v1/duAns", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -23,18 +25,33 @@ public class ApiDuAnController {
 	@Autowired
 	private DuAnModelService duAnModelService;
 	
+	@Autowired
+	private KeyApiService keyApiService;
+	
 	@GetMapping
-    public PagingObject<DuAnModel> findAll(Pageable pageable, @RequestParam(required = false, defaultValue = "") String keyWord) {
-        return duAnModelService.duAns(pageable, keyWord);
-    }
+	public ResponseEntity<PagingObject<DuAnModel>> findAll(Pageable pageable,
+			@RequestParam(required = false, defaultValue = "") String keyWord,
+			@RequestHeader(value = "authentication", required = true) String authentication) {
+		boolean checkQuyen = keyApiService.checkKey(authentication);
+		if (checkQuyen) {
+			return new ResponseEntity<>(duAnModelService.duAns(pageable, keyWord), HttpStatus.OK);
+		}
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+	}
 
 	@GetMapping("/{id}")
-	public ResponseEntity<DuAnModel> show(@PathVariable("id") Long id) {
-		DuAn duAn = duAnModelService.getById(id);
-		if (duAn != null) {
-			return new ResponseEntity<>(duAn.toDuAnModel(), HttpStatus.OK);
+	public ResponseEntity<DuAnModel> show(@PathVariable("id") Long id,
+			@RequestHeader(value = "authentication", required = true) String authentication) {
+		boolean checkQuyen = keyApiService.checkKey(authentication);
+		if (checkQuyen) {
+			DuAn duAn = duAnModelService.getById(id);
+			if (duAn != null) {
+				return new ResponseEntity<>(duAn.toDuAnModel(), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+			}
 		}
-		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
 	}
 	
 }
